bdc74d02430704e69b707b5e33370518bf7d7db6
Chris Parent <chrisnparent@gmail.com>
2020-06-25 19:29:04 -0400

    Add new traits

--- components/Traits.js
+++ components/Traits.js
@@ -3,9 +3,67 @@ class Traits extends Component {
         return String.raw`
             <div id="${this.id}" class="container-fluid">
                 <div class="row">
-                    ${this.state.map((x,i) => this.add(new Trait(`traits_${i}`, { trait: x, edit: false }))).reduce((a,b) => `${a}${b}`, '')}
+                    ${this.state.traits.map((x,i) => this.add(new Trait(`traits_${i}`, { trait: x, edit: false }))).reduce((a,b) => `${a}${b}`, '')}
+                    ${this.drawEditor()}
                 </div>
             </div>
         `;
+    }
+
+    drawEditor() {
+        if(this.state.traits.length == 4) return '';
+
+        if(this.state.edit) return String.raw`
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h2>New Trait</h2>
+                        <div class="input-group align-self-center mr-1">
+                            <input id="${this.id}_newTraitName" class="form-control">
+                            <div class="input-group-append">
+                                <button id="${this.id}_confirm" class="btn btn-success">&check;</button>
+                                <button id="${this.id}_cancel" class="btn btn-danger">&cross;</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        return String.raw`
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <button id="${this.id}_add" class="btn btn-success">Add trait</button>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
+    initialize() {
+        super.initialize();
+
+        $(`#${this.id}_add`).on('click touch', e => {
+            this.state.edit = true;
+            this.update();
+        });
+
+        $(`#${this.id}_confirm`).on('click touch', e => {
+            this.state.traits.push({ 
+                name: $(`#${this.id}_newTraitName`).val(), 
+                level: 1, 
+                used: 0, 
+                checks: 0 
+            });
+
+            this.state.edit = false;
+            this.update();
+        })
+
+        $(`#${this.id}_cancel`).on('click touch', e => {
+            this.state.edit = false;
+            this.update();
+        })
     }
 }
