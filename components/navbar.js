f433ce33d0e6e5aac76ba23ebd7de7da75b61ddd
Chris Parent <chrisnparent@gmail.com>
2020-06-28 20:30:54 -0400

    Alerts for i/o

--- components/navbar.js
+++ components/navbar.js
@@ -15,33 +15,46 @@ class Navbar extends Component{
 
     draw() {
         return String.raw`
-            <nav id="${this.id}" class="navbar navbar-expand-md navbar-light bg-light">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#${this.id}_nav">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div id="${this.id}_nav" class="collapse navbar-collapse">
-                    <ul class="navbar-nav mr-auto">
-                        ${this.tabs.map((t) => this.drawTab(t)).reduce((a,b) => `${a}${b}`)}
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" id="${this.id}_characters" data-toggle="dropdown" >Characters</a>
-                            <div class="dropdown-menu">
-                                ${this.characters.map(x => String.raw`<a href="#" data-character="${x}" class="dropdown-item">${x}</a>`).reduce((a,b) => `${a}${b}`, '')}
-                            </div>
-                        </li>
-                    </ul>
-                    <div class="navbar-nav">
-                        <a id="${this.id}_saveconfirm" class="nav-item nav-link text-success" hidden="hidden">${this.parent.state.bio.name} saved</a>
-                        <div class="nav-item dropdown">
-                            <button class="dropdown-toggle btn btn-light border border-dark" id="${this.id}_options" data-toggle="dropdown" >Options</button>
-                            <div class="dropdown-menu">
-                                <a id="${this.id}_save" href="#" class="dropdown-item">Save</a>
-                                <a id="${this.id}_export" href="#" class="dropdown-item">Export</a>
-                                <a id="${this.id}_import" href="#" class="dropdown-item">Import</a>
+            <div id="${this.id}">
+                <nav class="navbar navbar-expand-md navbar-light bg-light">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#${this.id}_nav">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div id="${this.id}_nav" class="collapse navbar-collapse">
+                        <ul class="navbar-nav mr-auto">
+                            ${this.tabs.map((t) => this.drawTab(t)).reduce((a,b) => `${a}${b}`)}
+                            <li class="nav-item dropdown">
+                                <a href="#" class="nav-link dropdown-toggle ${this.characters.length == 0 ? 'disabled' : ''}" id="${this.id}_characters" data-toggle="dropdown" >Characters</a>
+                                <div class="dropdown-menu">
+                                    ${this.characters.map(x => String.raw`<a href="#" data-character="${x}" class="dropdown-item">${x}</a>`).reduce((a,b) => `${a}${b}`, '')}
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="navbar-nav">
+                            <div class="nav-item dropdown">
+                                <button class="dropdown-toggle btn btn-light border border-dark" id="${this.id}_options" data-toggle="dropdown" >Options</button>
+                                <div class="dropdown-menu">
+                                    <a id="${this.id}_save" href="#" class="dropdown-item">Save</a>
+                                    <a id="${this.id}_export" href="#" class="dropdown-item">Export</a>
+                                    <a id="${this.id}_import" href="#" class="dropdown-item">Import</a>
+                                    <a id="${this.id}_delete" href="#" class="dropdown-item">Delete</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </nav>
+                </nav>
+                ${this.drawAlert()}
+            </div>
+        `;
+    }
+
+    drawAlert() {
+        if(!this.state.alert) return'';
+
+        return String.raw`
+            <div id="${this.id}_msg" class="alert alert-success btn w-100 text-left">
+                ${this.state.alert}
+            </div>
         `;
     }
 
@@ -59,30 +72,44 @@ class Navbar extends Component{
             }
 
             localStorage.setItem(this.parent.state.bio.name, JSON.stringify(this.parent.state));
-            this.update();
-
-            $(`#${this.id}_saveconfirm`).removeAttr('hidden');
+            this.state.alert = `${this.parent.state.bio.name} saved`;
+            this.parent.update();
         });
 
         $(`#${this.id}_export`).click(e => {
             let href = URL.createObjectURL(new Blob([JSON.stringify(this.parent.state)]));
             e.target.href = href;
-            e.target.download = `${this.parent.state.bio.name}.json`;
+            e.target.download = `${this.parent.state.bio.name}.tb2e`;
         });
 
         $(`#${this.id}_import`).click(e => {
-            let file = $('<input type="file">');
+            let file = $('<input type="file" accept=".tb2e">');
             file.change(this.load);
             file.click();
         });
 
+        $(`#${this.id}_delete`).click(e => {
+            if(!confirm(`Delete ${this.parent.state.bio.name}?`)) return;
+
+            localStorage.removeItem(this.parent.state.bio.name);
+            this.state.alert = `${this.parent.state.bio.name} deleted from character storage`;
+            this.parent.update();
+        });
+
+        $(`#${this.id}_msg`).click(e => {
+            this.state.alert = '';
+            this.update();
+        });
+
         this.find('[data-character]').click(e => {
             this.parent.state = JSON.parse(localStorage.getItem($(e.target).attr('data-character')));
+            this.parent.state.navbar.alert = `${this.parent.state.bio.name} opened`;
             this.parent.update();
         });
 
         this.find('[data-tab]').click(e => {
             this.state.tab = $(e.target).attr('data-tab');
+            this.state.alert = `${this.parent.state.bio.name} loaded`;
             this.parent.update();
         });
     }
@@ -91,7 +118,8 @@ class Navbar extends Component{
         e.target.files[0].text().then((t) => {
             let key = JSON.parse(t).bio.name;
             localStorage.setItem(key, t);
-            this.update();
+            this.state.alert = `${this.parent.state.bio.name} added to character storage`;
+            this.parent.update();
         });
     }
 }
