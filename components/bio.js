f7bf9770044295081a76e6222fa85bdcdb1c4c44
Chris Parent <chrisnparent@gmail.com>
2020-06-29 13:59:35 -0400

    Textarea size fixes

--- components/bio.js
+++ components/bio.js
@@ -34,14 +34,14 @@ class Bio extends Component {
     input(label, prop) {
         if(this.state.edit == prop) return String.raw`
             <div class="d-flex mb-1 border-bottom col-lg-3 col-md-4">
-                <span class="align-self-center text-right mr-1 py-2" style="width: 4em; height: 2.5em;">${label}</span>
+                <span class="align-self-center text-right mr-1 py-2 font-weight-bold" style="width: 4.5em; height: 2.5em;">${label}</span>
                 <input data-input="${prop}" class="flex-grow-1 form-control" value="${this.state[prop]}">
             </div>
         `;
 
         return String.raw`
             <div class="d-flex mb-1 border-bottom col-lg-3 col-md-4">
-                <span class="align-self-center text-right border-right pr-1 py-2" style="width: 4em;">${label}</span>
+                <span class="align-self-center text-right border-right pr-1 py-2 font-weight-bold" style="width: 4.5em;">${label}</span>
                 <button data-edit="${prop}" class="flex-grow-1 btn btn-light text-left">${this.state[prop]}</button>
             </div>
         `;
@@ -50,15 +50,15 @@ class Bio extends Component {
     textArea(label, prop) {
         if(this.state.edit == prop) return String.raw`
             <div class="d-flex flex-column mb-1 col-lg-3 col-md-4">
-                <span class="py-2 border-bottom">${label}</span>
+                <span class="py-2 border-bottom font-weight-bold">${label}</span>
                 <textarea data-input="${prop}" class="flex-grow-1 form-control">${this.state[prop]}</textarea>
             </div>
         `;
 
         return String.raw`
             <div class="d-flex flex-column mb-1 col-lg-3 col-md-4">
-                <span class="py-2 border-bottom">${label}</span>
-                <button data-edit="${prop}" class="flex-grow-1 btn btn-light text-left" style="min-height: 2.5em;">${this.state[prop]}</button>
+                <span class="py-2 border-bottom font-weight-bold">${label}</span>
+                <button data-edit="${prop}" class="btn btn-light text-left align-top wrap" style="min-height: 2.5em;">${this.state[prop]}</button>
             </div>
         `;
     }
@@ -76,6 +76,10 @@ class Bio extends Component {
             this.state[prop] = $(e.target).val();
             this.state.edit = false;
             this.update();
+        });
+
+        this.find('textarea').keyup(e => {
+            e.target.style.height = `${e.target.scrollHeight + 2}px`;
         });
 
         if(this.state.edit) this.find('[data-input]').focus();
