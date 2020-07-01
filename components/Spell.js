57ca7d730712f2bb83e86835bd7cdc2c37cc5f76
Chris Parent <chrisnparent@gmail.com>
2020-07-01 12:45:07 -0400

    Memory palace

--- components/Spell.js
+++ components/Spell.js
@@ -9,16 +9,18 @@ class Spell extends Component{
     ]
 
     draw() {
+        if(this.isHidden()) return '';
+
         return String.raw`
             <div id="${this.id}" class="col-md-6">
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex">
                             <h4 class="flex-grow-1"><button class="badge btn btn-light w-100 text-left">${this.state.spell.name}</button></h4>
-                            <button class="align-self-center badge btn ${this.state.memorized ? 'btn-dark' : 'btn-light border'} ml-1 p-2">&check;</button>
+                            ${this.drawMemorized()}
                         </div>
                         <div class="d-flex">
-                            <h5><button class="badge btn btn-dark w-100 text-left">${this.circles[this.state.spell.circle - 1]}</button></h5>
+                            <h5><button data-circle="" class="badge btn btn-dark w-100 text-left">${this.circles[this.state.spell.circle - 1]}</button></h5>
                         </div>
                     </div>
                 </div>
@@ -26,7 +28,37 @@ class Spell extends Component{
         `;
     }
 
-    textarea() {
+    drawMemorized() {
+        if(!this.state.canMemorize && !this.state.spell.memorized) return '';
 
+        return String.raw`
+            <button data-memorized="" class="align-self-center badge btn ${this.state.spell.memorized ? 'btn-dark' : 'btn-light border'} ml-1 p-2">&check;</button>
+        `;
+    }
+
+    isHidden() {
+        if(this.state.show == 'memory' && !this.state.spell.memorized) return true;
+
+        return false;
+    }
+
+    textarea() {
+    }
+
+    initialize() {
+        super.initialize();
+
+        this.find('[data-circle]').click(e => {
+            this.state.spell.circle += e.originalEvent.shiftKey ? -1 : 1;
+            if(this.state.spell.circle < 1) this.state.spell.circle = this.circles.length;
+            if(this.state.spell.circle > this.circles.length) this.state.spell.circle = 1;
+
+            this.parent.update(); // Impacts memory palace
+        });
+
+        this.find('[data-memorized]').click(e => {
+            this.state.spell.memorized = !this.state.spell.memorized;
+            this.parent.update(); // Impacts memory palace
+        });
     }
 }
--- components/Spells.js
+++ components/Spells.js
@@ -13,11 +13,25 @@ class Spells extends Component {
                                     <span class="align-self-center btn badge-light border">${this.space()}</span>
                                     <span class="align-self-center mx-1">/</span>
                                     <button id="${this.id}_memory" class="align-self-center btn btn-dark">${this.state.memory}</button>
-                                    <button id="${this.id}_add" class="align-self-center btn btn-light border ml-auto">Add spell</button>
+                                </div>
+                                <div class="d-flex">
+                                    <div class="dropdown">
+                                        <button class="dropdown-toggle btn btn-light border mb-1 mr-1" data-toggle="dropdown" >Show</button>
+                                        <div class="dropdown-menu">
+                                            <button data-show-skills="all" class="dropdown-item ${this.styleForShow('all')}">All</button>
+                                            <button data-show-skills="memory" class="dropdown-item ${this.styleForShow('memory')}">Memorized</button>
+                                        </div>
+                                    </div>
+                                    <button id="${this.id}_add" class="btn btn-light border mb-1 mr-1">Add spell</button>
                                 </div>
                                 <div class="row mt-2">
                                     ${this.state.spells
-                                        .map((x,i) => this.add(new Spell(`${this.id}_spells_${i}`, { spell: x, edit: false })))
+                                        .map((x,i) => this.add(new Spell(`${this.id}_spells_${i}`, { 
+                                            spell: x, 
+                                            show: this.state.show,
+                                            canMemorize: this.space() >= x.circle,
+                                            edit: false 
+                                        })))
                                         .reduce((a,b) => `${a}${b}`, '')}
                                 </div>
                             </div>
@@ -29,11 +43,18 @@ class Spells extends Component {
     }
 
     space = () => {
-        return this.state.memory - this.state.spells.reduce((a,b) => a + b.memorized ? b.circle : 0, 0)
+        return this.state.memory - this.state.spells.reduce((a,b) => a + (b.memorized ? b.circle : 0), 0)
     }
+
+    styleForShow = (value) => this.state.show == value ? 'bg-dark text-light' : '';
 
     initialize() {
         super.initialize();
+
+        this.find('[data-show-skills]').click(e => {
+            this.state.show = $(e.target).data('show-skills');
+            this.update();
+        });
 
         $(`#${this.id}_add`).click(e => {
             this.state.spells.push({
