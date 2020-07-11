60fd986995ef1f0fc93f1e05a7ff1da59f51f29d
Chris Parent <chrisnparent@gmail.com>
2020-07-11 00:28:36 -0400

    fix fate and persona requirements

--- components/Advancement.js
+++ components/Advancement.js
@@ -4,13 +4,13 @@ class Advancement extends Component {
         { level: 1, fate: 0, persona: 0 },
         { level: 2, fate: 3, persona: 3 },
         { level: 3, fate: 7, persona: 6 },
-        { level: 4, fate: 13, persona: 9 },
-        { level: 5, fate: 22, persona: 12 },
-        { level: 6, fate: 31, persona: 16 },
-        { level: 7, fate: 41, persona: 20 },
-        { level: 8, fate: 52, persona: 24 },
-        { level: 9, fate: 64, persona: 28 },
-        { level: 10, fate: 77, persona: 32 }
+        { level: 4, fate: 14, persona: 12 },
+        { level: 5, fate: 22, persona: 20 },
+        { level: 6, fate: 31, persona: 30 },
+        { level: 7, fate: 41, persona: 42 },
+        { level: 8, fate: 52, persona: 56 },
+        { level: 9, fate: 64, persona: 72 },
+        { level: 10, fate: 78, persona: 98 }
     ]
 
     draw() {
