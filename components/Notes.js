b42fe5fe966588ea635c72fd4ed8dfa8b3adc3e1
Chris Parent <chrisnparent@gmail.com>
2022-05-13 20:23:34 -0400

    note sorting

--- components/Notes.js
+++ components/Notes.js
@@ -26,7 +26,17 @@ class Notes extends Component{
 
     drawControls() {
         return String.raw`
-            <button id="${this.id}_add" class="btn btn-light border">Add note</button>
+            <div class="d-flex">
+                <button id="${this.id}_add" class="btn btn-light border mb-1 mr-1">Add note</button>
+                <div class="dropdown">
+                    <button class="dropdown-toggle btn btn-light border mb-1" data-toggle="dropdown" >Sort</button>
+                    <div class="dropdown-menu">
+                        <button data-sort="newest" class="dropdown-item">Newest</button>
+                        <button data-sort="oldest" class="dropdown-item">Oldest</button>
+                        <button data-sort="alpha" class="dropdown-item">A &rarr; Z</button>
+                    </div>
+                </div>
+            </div>
             <div class="d-flex">
                 <input id="${this.id}_filter" class="form-control" placeholder="filter" value="${this.state.filter}">
             </div>
@@ -42,6 +52,16 @@ class Notes extends Component{
 
     initialize() {
         super.initialize();
+
+        _(`#${this.id} [data-sort]`).map((x) => x.onclick = e => {
+            let sortMethod = e.target.dataset.sort;
+            if (sortMethod == 'alpha') this.state.notes.sort((a,b) => a.title.localeCompare(b.title));
+            else if (sortMethod == 'oldest') this.state.notes.sort((a,b) => a.date < b.date);
+            else if (sortMethod == 'newest') this.state.notes.sort((a,b) => a.date > b.date);
+
+            this.state.sort = sortMethod;
+            this.update();
+        });
 
         _(`#${this.id}_add`)[0].onclick = e => {
             this.state.notes.splice(0, 0, {
